4cdfc2eee507901b433a4f4df0af1109f73f5140
R. Zander <hello@rza.io>
2018-10-22 23:34:07 +0200

    Updated bash script

--- vid/generateSpriteSheet.sh
+++ vid/generateSpriteSheet.sh
@@ -3,3 +3,4 @@
 ffmpeg -i "example.mp4" -f image2 -vf fps=fps=10 img%03d.jpg
 files=$(ls img*.jpg | sort -t '-' -n -k 2 | tr '\n' ' ')
 convert $files -append output.jpg
+find . -name 'img*.jpg' #-delete
